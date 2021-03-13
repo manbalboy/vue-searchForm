ff996d083f48cafb0a8a6c51076030c94d1f6174
manbalboy <manbalboy@hanmail.net>
2021-03-14 01:30:40 +0900

    탭뷰 구현

--- js-version/js/controllers/MainController.js
+++ js-version/js/controllers/MainController.js
@@ -12,7 +12,8 @@ export default {
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm());
 
-        TabView.setup(document.querySelector('#tabs'));
+        TabView.setup(document.querySelector('#tabs'))
+            .on('@change', e => this.onChangeTab(e.detail.tabName));
 
         ResultView.setup(document.querySelector('#search-result'));
         
@@ -47,4 +48,9 @@ export default {
     onSearchResult(data) {
         ResultView.render(data);
     },
+
+    onChangeTab(tabName) {
+        console.log(tag, 'onChangeTab()');
+        
+    }
 }
--- js-version/js/views/TabView.js
+++ js-version/js/views/TabView.js
@@ -1,19 +1,39 @@
-import View from './View.js';
+import View from './View.js'
 
-const tag = '[TabView]';
+const tag = '[TabView]'
 
-const TabView = Object.create(View);
+const TabView = Object.create(View)
+
+TabView.tabNames = {
+  recommand: '추천 검색어',
+  recent: '최근 검색어',
+}
 
 TabView.setup = function (el) {
-    this.init(el);
-    return this;
+  this.init(el);
+  this.bindClick();
+  return this;
 }
 
 TabView.setActiveTab = function (tabName) {
-    Array.from(this.el.children).forEach(li => {
-      li.className = li.innerHTML === tabName ? 'active' : ''
-    })
-    this.show()
-  }
+  Array.from(this.el.children).forEach(li => {
+    li.className = li.innerHTML === tabName ? 'active' : ''
+  })
+  this.show()
+}
 
-  export default TabView;
+TabView.bindClick  = function () {
+    Array.from(this.el.querySelectorAll('li'))
+            .forEach(
+                li => {
+                    li.addEventListener('click', e => this.onClick(li.innerHTML));
+                }
+            )
+}
+
+TabView.onClick = function (tabName) {
+    this.setActiveTab(tabName);
+    this.emit('@change', tabName);
+}
+
+export default TabView
