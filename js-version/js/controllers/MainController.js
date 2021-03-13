88932048aa9e05c744753b23fad42219562591a8
manbalboy <manbalboy@hanmail.net>
2021-03-14 01:42:09 +0900

    추천검색어 DATA 랜더링

--- js-version/js/controllers/MainController.js
+++ js-version/js/controllers/MainController.js
@@ -1,8 +1,10 @@
 import FormView from '../views/FormView.js';
 import ResultView from '../views/ResultView.js';
 import TabView from '../views/TabView.js';
+import KeywordView from '../views/KeywordView.js';
 
 import SearchModel from '../models/SearchModel.js';
+import KeywordModel from '../models/KeywordModel.js';
 const tag = '[MainController]';
 
 export default {
@@ -15,15 +17,29 @@ export default {
         TabView.setup(document.querySelector('#tabs'))
             .on('@change', e => this.onChangeTab(e.detail.tabName));
 
+        KeywordView.setup(document.querySelector('#search-keyword'));
         ResultView.setup(document.querySelector('#search-result'));
+
         
         this.selectedTab = '추천 검색어';
         this.renderView();
 
     },
+
     renderView() {
         console.log(tag, 'renderView()');
         TabView.setActiveTab(this.selectedTab);
+
+
+        if(this.selectedTab === "추천 검색어") {
+            KeywordModel.list().then(data => {
+                KeywordView.render(data);
+            })
+        } else {
+
+        }
+
+
         ResultView.hide();
     },
 
