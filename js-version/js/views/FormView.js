6569e13cf6959ec1a5f9c177da49025a758df0e2
manbalboy <manbalboy@hanmail.net>
2021-03-14 00:06:26 +0900

    검색어를 입력하면 x 버튼 보임 구현

--- js-version/js/views/FormView.js
+++ js-version/js/views/FormView.js
@@ -8,11 +8,20 @@ FormView.setup = function(el) {
     this.init(el);
     this.inputEl = el.querySelector('[type=text]');
     this.resetEl = el.querySelector('[type=reset]');
+    this.bindEvents();
     this.showResetBtn(false);
 }
 
 FormView.showResetBtn = function (show = true) {
     this.resetEl.style.display = show ? 'block' : 'none';
+}
+
+FormView.bindEvents = function (){
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e));
+};
+
+FormView.onKeyup = function () {
+    this.showResetBtn(this.inputEl.value.length);
 }
 
 export default FormView;
